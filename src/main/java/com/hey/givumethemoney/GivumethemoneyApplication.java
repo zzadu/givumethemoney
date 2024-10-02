34c70725c8cc4f5dc923c29eb0b1085e654c56ea
zzadu <zzadu08@naver.com>
2024-10-02 21:57:09 +0900

    Initialize Project

---
+++ src/main/java/com/hey/givumethemoney/GivumethemoneyApplication.java
@@ -0,0 +1,13 @@
+package com.hey.givumethemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivumethemoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivumethemoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hey/givumethemoney/GivumethemoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hey.givumethemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GivumethemoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
